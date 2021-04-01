f81dc78187e599b393c1a6f0e79c3f6c3409c72d
Nirvana <1123151343@qq.com>
2021-04-01 17:06:23 +0800

    format the Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -1,19 +1,18 @@
 import h5py
 import numpy as np
 
+
 # F:\fsociety\graduation_project\Project\Copy\feature_label\labels_urfd_tf.h5
-class VisualizerDiy:
-    def visualize_data(self, file_path):
-        h5_file = h5py.File(file_path, "r")  # open h5 file
-        print(h5_file.keys())
-        for key in h5_file.keys():
-            print("Shape:" + str(h5_file[key].shape))
+def visualize_data(file_path):
+    h5_file = h5py.File(file_path, "r")  # open h5 file
+    print(h5_file.keys())
+    for key in h5_file.keys():
+        print("Shape:" + str(h5_file[key].shape))
 
 
 def main():
-    visualizer = VisualizerDiy()
     h5_file_path = input("Where's your hdf5 file?")
-    visualizer.visualize_data(str(h5_file_path))
+    visualize_data(str(h5_file_path))
 
 
 if __name__ == "__main__":
